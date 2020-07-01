1f2960f1983be0e7f1394515f68c51447b08f5d5
Ash-Shang <ash159@qq.com>
2020-07-01 18:08:51 +0800

    mybatis源码注释

---
+++ src/main/java/org/apache/ibatis/reflection/factory/ObjectFactory.java
@@ -0,0 +1,48 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.reflection.factory;
+
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * MyBatis uses an ObjectFactory to create all needed new Objects.
+ *
+ * @author Clinton Begin
+ */
+public interface ObjectFactory {
+
+  /**
+   * 设置 Properties
+   */
+  void setProperties(Properties properties);
+
+  /**
+   * 创建指定类的对象，使用默认构造方法
+   */
+  <T> T create(Class<T> type);
+
+  /**
+   *  创建指定类的对象，使用特定的构造方法
+   */
+  <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs);
+
+  /**
+   *  判断指定类是否为集合类
+   */
+  <T> boolean isCollection(Class<T> type);
+
+}
